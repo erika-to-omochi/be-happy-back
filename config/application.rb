b27b5ea709660114d2a9673d50bb851c81a977e7
Erika Komori <komoriakirarika@example.com>
2024-10-09 19:50:50 +0900

    バックエンドの環境構築

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative "boot"
+
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module App
+  class Application < Rails::Application
+    config.load_defaults 7.0
+    config.api_only = true
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+  end
+end
