a9549fec88947a1013de544c38fc339d0aea8cb6
Erika Komori <komoriakirarika@example.com>
2024-10-21 11:38:51 +0900

    ログインno

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8000', '127.0.0.1:8000', 'https://be-happy-front.vercel.app'
 
-    resource '/api/*',
+    resource '*',
       headers: :any,
       methods: :any,
       expose: %w(Authorization),
