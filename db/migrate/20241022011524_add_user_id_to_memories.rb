9bbb5be77fdc0d0c5b21adcec8897279f1374659
Erika Komori <komoriakirarika@example.com>
2024-10-23 11:46:01 +0900

    マイグレーションファイルの修正

--- db/migrate/20241022011524_add_user_id_to_memories.rb
+++ db/migrate/20241022011524_add_user_id_to_memories.rb
@@ -1,6 +1,7 @@
 class AddUserIdToMemories < ActiveRecord::Migration[7.0]
   def change
     unless column_exists?(:memories, :user_id)
+      Memory.where(user_id: nil).update_all(user_id: 1)
       add_reference :memories, :user, null: false, foreign_key: true
     end
   end
