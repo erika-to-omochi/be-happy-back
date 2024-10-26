d21e823a8fd1edf37ecf289319a1a264b32b9523
Erika Komori <komoriakirarika@example.com>
2024-10-26 16:49:53 +0900

    ゲストログインに対応

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,4 +1,5 @@
 class Memory < ApplicationRecord
   belongs_to :user, optional: true
   validates :content, presence: true, length: { maximum: 1000 }
+  validates :name, presence: true, length: { maximum: 50 }
 end
